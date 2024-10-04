4d64a54c70ba545f093656929afa3331f223dad9
jobwayne <otienojobwayneotieno@gmail.com>
2024-10-04 20:54:15 +0300

    arrow

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide (a, b){
+    return (a/b);
+}
+divide(2000, 100);
+const square = parameter => parameter*parameter
+const add= (parameter1 , parameter2) => {
+    return (parameter1+parameter2)
+}
